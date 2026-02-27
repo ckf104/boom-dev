667c7f3e7b56a39efd87b31419d8692bc920ac67
ckf104 <1900011634@pku.edu.cn>
2026-02-27 16:01:38 +0800

    configs: 增加空的 LimitPrefetchDist 配置

--- src/main/scala/v3/common/config-mixins.scala
+++ src/main/scala/v3/common/config-mixins.scala
@@ -51,6 +51,8 @@ class PfMSHRNumber(n: Int) extends Config((site, here, up) => {
   }
 })
 
+class LimitPrefetchDist(n: Int) extends Config((_,_,_) => PartialFunction.empty)
+
 class EnableBoomFlushGHistRestore extends Config((site, here, up) => {
   case TilesLocated(InSubsystem) => up(TilesLocated(InSubsystem), site) map {
     case tp: BoomTileAttachParams => tp.copy(tileParams = tp.tileParams.copy(core = tp.tileParams.core.copy(
