e93f91241423788d439031e70f399c80dc190544
ckf104 <1900011634@pku.edu.cn>
2026-03-13 20:26:31 +0800

    configs: 增加 ftq 和 icache 大小的配置

--- src/main/scala/v3/common/config-mixins.scala
+++ src/main/scala/v3/common/config-mixins.scala
@@ -34,6 +34,25 @@ class DCacheConfigs(nSet: Int, nWay: Int, nTLB: Int) extends Config((site, here,
   }
 })
 
+class ICacheConfigs(nWay: Int) extends Config((site, here, up) => {
+  case TilesLocated(InSubsystem) => up(TilesLocated(InSubsystem), site) map {
+    case tp: BoomTileAttachParams => tp.copy(tileParams = tp.tileParams.copy(icache = Some(tp.tileParams.icache.get.copy(
+      nWays = nWay
+    ))))
+    case other => other
+  }
+})
+
+class FTQConfigs(nEntries: Int) extends Config((site, here, up) => {
+  case TilesLocated(InSubsystem) => up(TilesLocated(InSubsystem), site) map {
+    case tp: BoomTileAttachParams => tp.copy(tileParams = tp.tileParams.copy(core = tp.tileParams.core.copy(
+      ftq = tp.tileParams.core.ftq.copy(nEntries = nEntries)
+    )))
+    case other => other
+  }
+})
+
+
 class BusWidthConfigs(busWidth: Int) extends Config((site, here, up) => {
   case TilesLocated(InSubsystem) => up(TilesLocated(InSubsystem), site) map {
     case tp: BoomTileAttachParams => tp.copy(tileParams = tp.tileParams.copy(
